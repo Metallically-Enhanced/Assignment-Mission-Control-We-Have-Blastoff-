5ee3ca8b93da7c3e35be996f37398e8972309f3a
Amasa Stice <97764274+Metallically-Enhanced@users.noreply.github.com>
2022-01-25 17:41:34 -0500

    Add files via upload
    My submission for this assignment shows a functional countdown timer activated by a button.

---
+++ LaunchButton.js
@@ -0,0 +1,73 @@
+function StartTheCountdown(){
+    //This function initiates the launch sequence by way of the "StartTheCountdown" function being called via the onclick event trigger linked to the button on line 16 of the.
+//Declaration of the variables occurs below.
+var countdown = 10;
+//Used to keep track of real time.
+//Time unit used is milliseconds.
+var timeout = 10000
+setTimeout(() => {
+    document.getElementById("CountdownDisplay").innerHTML = "BLASTOFF";
+    countdown = countdown - 1;
+}, timeout)
+timeout = timeout - 1000;
+//Time until launch is decremented by one each time this script is called
+setTimeout(() => {
+    document.getElementById("CountdownDisplay").innerHTML = countdown;
+    countdown = countdown - 1;
+}, timeout)
+timeout = timeout - 1000;
+//Time until launch is decremented by one each time this script is called
+setTimeout(() => {
+    document.getElementById("CountdownDisplay").innerHTML = countdown;
+    countdown = countdown - 1;
+}, timeout)
+timeout = timeout - 1000;
+//Time until launch is decremented by one each time this script is called
+setTimeout(() => {
+    document.getElementById("CountdownDisplay").innerHTML = countdown;
+    countdown = countdown - 1;
+}, timeout)
+timeout = timeout - 1000;
+//Time until launch is decremented by one each time this script is called
+setTimeout(() => {
+    document.getElementById("CountdownDisplay").innerHTML = countdown;
+    countdown = countdown - 1;
+}, timeout)
+timeout = timeout - 1000;
+//Time until launch is decremented by one each time this script is called
+setTimeout(() => {
+    document.getElementById("CountdownDisplay").innerHTML = countdown;
+    countdown = countdown - 1;
+}, timeout)
+timeout = timeout - 1000;
+//Time until launch is decremented by one each time this script is called
+setTimeout(() => {
+    document.getElementById("CountdownDisplay").innerHTML = countdown;
+    countdown = countdown - 1;
+}, timeout)
+timeout = timeout - 1000;
+//Time until launch is decremented by one each time this script is called
+setTimeout(() => {
+    document.getElementById("CountdownDisplay").innerHTML = countdown;
+    countdown = countdown - 1;
+}, timeout)
+timeout = timeout - 1000;
+//Time until launch is decremented by one each time this script is called
+setTimeout(() => {
+    document.getElementById("CountdownDisplay").innerHTML = countdown;
+    countdown = countdown - 1;
+}, timeout)
+timeout = timeout - 1000;
+//Time until launch is decremented by one each time this script is called
+setTimeout(() => {
+    document.getElementById("CountdownDisplay").innerHTML = countdown;
+    countdown = countdown - 1;
+}, timeout)
+timeout = timeout - 1000;
+//Time until launch is decremented by one each time this script is called
+setTimeout(() => {
+    document.getElementById("CountdownDisplay").innerHTML = countdown;
+    countdown = countdown - 1;
+}, timeout)
+timeout = timeout - 1000;}
+//Time until launch is decremented by one each time this script is called
